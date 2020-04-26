2177db32dec9bdb8deb228dc741a4cb11572d8fb
tocalopes <tocalopes@users.noreply.github.com>
2020-04-26 20:55:04 -0300

    Criação do botão flutuante e começo de implementação da listview

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,114 +1,101 @@
-import 'dart:html';
+import 'dart:async';
 
-import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-final String contactTable = "contact";
-final String idColumn = "id_contact";
-final String nameColumn = "name";
-final String emailColumn = "email";
-final String phoneColumn = "phone";
-final String imgColumn = "img";
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
 
-
-class ContactHelper{
+class ContactHelper {
 
   static final ContactHelper _instance = ContactHelper.internal();
+
   factory ContactHelper() => _instance;
 
   ContactHelper.internal();
+
   Database _db;
 
   Future<Database> get db async {
-    if(db != null){
+    if(_db != null){
       return _db;
-    }else{
+    } else {
       _db = await initDb();
       return _db;
     }
   }
 
-  Future<Database> initDb() async{
+  Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
 
-    return openDatabase(path, version: 1, onCreate: (Database db,int newerVersion) async{
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
       await db.execute(
-        "CREATE TABLE $contactTable ("
-          "$idColumn INTEGER PRIMERY KEY,"
-          "$nameColumn TEXT,"
-          "$emailColumn TEXT,"
-          "$phoneColumn TEXT,"
-          "$imgColumn TEXT"
-        ");"
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+            "$phoneColumn TEXT, $imgColumn TEXT)"
       );
     });
   }
 
   Future<Contact> saveContact(Contact contact) async {
-      Database dbContact = await db ;
-      contact.id = await dbContact.insert(contactTable,contact.toMap());
-      return contact;
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
   }
 
-  Future<Contact> getContactById(int id) async{
-    Database dbContact = await db ;
-    List<Map> maps = await dbContact.query(
-      contactTable,
-      columns: [idColumn,nameColumn,emailColumn,phoneColumn,imgColumn],
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
       where: "$idColumn = ?",
-      whereArgs: [id],
-    ); 
-    if (maps.length > 0){
+      whereArgs: [id]);
+    if(maps.length > 0){
       return Contact.fromMap(maps.first);
-    }else{
+    } else {
       return null;
     }
   }
 
-  Future<int> deleteContact(int id) async{
+  Future<int> deleteContact(int id) async {
     Database dbContact = await db;
-    return await dbContact.delete(
-      contactTable,
-      where: "$idColumn = ?",
-      whereArgs: [id];
-    );
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
   Future<int> updateContact(Contact contact) async {
     Database dbContact = await db;
     return await dbContact.update(contactTable,
-      contact.toMap(),
-      where: "$idColumn = ?",
-      whereArgs: [contact.id]);
+        contact.toMap(),
+        where: "$idColumn = ?",
+        whereArgs: [contact.id]);
   }
 
-  Future<List> getAllContacts() async{
+  Future<List> getAllContacts() async {
     Database dbContact = await db;
     List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
-
-    List<Contact> allContacts = List();
-
-    for(Map i in listMap){
-      allContacts.add(Contact.fromMap(i));
+    List<Contact> listContact = List();
+    for(Map m in listMap){
+      listContact.add(Contact.fromMap(m));
     }
-    return allContacts;
+    return listContact;
   }
 
-  Future<int> getNumber() async{
+  Future<int> getNumber() async {
     Database dbContact = await db;
     return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
-  Future<void> close() async{
+  Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
 
 }
 
-
-class Contact{
+class Contact {
 
   int id;
   String name;
@@ -116,31 +103,32 @@ class Contact{
   String phone;
   String img;
 
-  //Construtor chama from map
+  Contact();
+
   Contact.fromMap(Map map){
-    this.id = map[idColumn];
-    this.name = map[nameColumn];
-    this.email = map[emailColumn];
-    this.phone = map[phoneColumn];
-    this.img = map[imgColumn];
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
   }
 
-  Map toMap(){
-    Map<String,dynamic> map ={
+  Map toMap() {
+    Map<String, dynamic> map = {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
-      imgColumn: img,
+      imgColumn: img
     };
-    if (id != null){
+    if(id != null){
       map[idColumn] = id;
     }
     return map;
   }
 
   @override
-  String toString(){
-    return "Contact(id:$id, name:$name, email:$email, phone:$phone, img:$img)";
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
   }
 
 }
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper(); 
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+   helper.getAllContacts().then((list){
+     setState(() {
+      contacts = list;  
+     });
+   });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+        ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(8.0),
+          itemCount: contacts.length,
+          itemBuilder: (context,index){
+            
+          }
+        )
+      );
+  }
+}
