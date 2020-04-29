75d32828da23940a66e47698dfa41dcf2c277bd5
tocalopes <tocalopes@users.noreply.github.com>
2020-04-29 19:58:16 -0300

    Adicionando opções do contato

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -84,7 +84,9 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ),
-        onTap:(){_showContactPage(contact: contacts[index]);},
+        onTap:(){
+          _showOptions(context,index);
+        },
       );
     }
 
@@ -109,4 +111,61 @@ class _HomePageState extends State<HomePage> {
         });
       });
     } 
+
+    void _showOptions(BuildContext context, int index){
+      showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return BottomSheet(
+            onClosing: (){},
+            builder: (context){
+              return Container(
+                padding: EdgeInsets.all(10.0),
+                child:Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        child:Text("Ligar",
+                          style:TextStyle(color:Colors.red,fontSize: 20.0)),
+                        onPressed: (){
+
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        child:Text("Editar",
+                          style:TextStyle(color:Colors.red,fontSize: 20.0)),
+                        onPressed: (){
+                          Navigator.pop(context);
+                          _showContactPage(contact: contacts[index]);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        child:Text("Excluir",
+                          style:TextStyle(color:Colors.red,fontSize: 20.0)),
+                        onPressed: (){
+                          helper.deleteContact(contacts[index].id);
+                          setState(() {
+                            contacts.removeAt(index);
+                            Navigator.pop(context);
+                          });
+
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      );
+    }
 }
